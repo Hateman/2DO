18d15b038c592dc7217cf7ec8c1dcc2845ba7452
Alex <37542088+Hateman@users.noreply.github.com>
2019-03-12 15:16:50 +0200

    Add files via upload

---
+++ Scripts/1.js
@@ -0,0 +1,28 @@
+﻿new Vue ({
+  el: "#app1",
+  data: {
+    title: "2DO Task list",
+    items: [
+     
+    ]
+  },
+
+  methods: {
+
+    addItem: function () {
+      var input = document.getElementById('itemForm');
+      if (input.value !== '') {
+        this.items.push({
+          text:input.value
+        })
+        input.value = "";
+      }
+    },
+
+    deleteItem: function(index) {
+      this.items.splice(index, 1);
+    }
+  }
+    
+});
+
